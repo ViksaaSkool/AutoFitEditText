57afef7520de6f04252920a568be5bad28b4b75e
Viktor Arsovski <viktor.arsovski@propeople.dk>
2015-10-30 01:16:31 +0100

    Initial commit

---
+++ app/src/main/java/com/viksaa/autofit/et/MainActivity.java
@@ -0,0 +1,66 @@
+package com.viksaa.autofit.et;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.autofit.et.lib.AutoFitEditText;
+import com.autofit.et.lib.AutoFitEditTextUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.rET)
+    AutoFitEditText mAutoFitEditText;
+    @Bind(R.id.root)
+    CoordinatorLayout mRootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+        initAutoFitEditText();
+    }
+
+
+    public void initAutoFitEditText() {
+
+        mAutoFitEditText.setEnabled(true);
+        mAutoFitEditText.setFocusableInTouchMode(true);
+        mAutoFitEditText.setFocusable(true);
+        mAutoFitEditText.setEnableSizeCache(false);
+        //might cause crash on some devices
+        mAutoFitEditText.setMovementMethod(null);
+        // can be added after layout inflation;
+        mAutoFitEditText.setMaxHeight(330);
+        //don't forget to add min text size programmatically
+        mAutoFitEditText.setMinTextSize(60f);
+
+        AutoFitEditTextUtil.setNormalization(this, mRootView, mAutoFitEditText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
